fcbd31649c5e0c14332b47aa3087ee0b6e52eba1
FreeNikita <freenikita95@gmail.com>
2020-04-05 00:45:21 +0300

    add functional with basic react-navigation

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Platform } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { MainScreen } from "../screens/MainSrceen";
+import { PostScreen } from "../screens/PostScreen";
+import { AboutScreen } from "../screens/AboutScreen";
+import { BookedScreen } from "../screens/BookedScreen";
+import { CreateScreen } from "../screens/CreateScreen";
+
+import {THEME} from "../theme";
+
+const Stack = createStackNavigator();
+
+const screenOptionsIOS = {
+  headerStyle: {
+    backgroundColor: THEME.WHITE_COLOR,
+  },
+  headerTintColor: THEME.MAIN_COLOR
+};
+
+const screenOptionsAndroid = {
+  headerStyle: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+  headerTintColor: THEME.WHITE_COLOR
+};
+
+export function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="MainScreen"
+        screenOptions={Platform.OS === 'android' ? screenOptionsAndroid : screenOptionsIOS}>
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen name="PostScreen" component={PostScreen} />
+        <Stack.Screen name="AboutScreen" component={AboutScreen} />
+        <Stack.Screen name="BookedScreen" component={BookedScreen} />
+        <Stack.Screen name="CreateScreen" component={CreateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+export const THEME = {
+  MAIN_COLOR: '#303f9f',
+  DANGER_COLOR: '#d91b60',
+  WHITE_COLOR: '#fff',
+  REGULAT_FONT: 'open-regular',
+  BOLD_FONT: 'open-bold'
+};
